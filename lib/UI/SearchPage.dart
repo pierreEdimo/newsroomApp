7220113192e3aabcec3a5b14aca17d2a2a143f11
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-17 19:59:49 +0200

    i created a page to search for doctors

---
+++ lib/UI/SearchPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: FaIcon(
+            FontAwesomeIcons.arrowLeft,
+            color: Colors.black,
+            size: 18,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: new TextField(
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              hintText: "Search for a doctor",
+              hintStyle: TextStyle(color: Colors.black, fontSize: 22.0),
+            )),
+      ),
+      body: Center(
+        child: Text("Hello Search for a doctor"),
+      ),
+    );
+  }
+}
