0e6538722d310ef068c49bb20506dfc2a20f8ff4
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-20 17:04:53 +0200

    i created the search page

--- lib/UI/SearchPage.dart
+++ lib/UI/SearchPage.dart
@@ -1,5 +1,7 @@
+import 'package:findadoctor/Model/DoctorModel.dart';
+import 'package:findadoctor/Service/DoctorService.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -7,29 +9,58 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final DoctorService doctorService = new DoctorService();
+  List<Doctor> doctors = List();
+  List<Doctor> filteredDoctors = List();
+
+  @override
+  void initState() {
+    super.initState();
+    doctorService.getAllDoctors().then((doctorFromServer) {
+      setState(() {
+        doctors = doctorFromServer;
+        filteredDoctors = doctors;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: FaIcon(
-            FontAwesomeIcons.arrowLeft,
-            color: Colors.black,
-            size: 18,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
+      body: Container(
+        padding: EdgeInsets.only(top: 52.5, left: 20, right: 20),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(15.0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  hintText: "Search a Doctor",
+                  hintStyle: TextStyle(fontSize: 20, color: Colors.black)),
+              onChanged: (string) {
+                setState(() {
+                  filteredDoctors = doctors
+                      .where((u) =>
+                          (u.city.toLowerCase().contains(string.toLowerCase())))
+                      .toList();
+                });
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredDoctors.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Text(filteredDoctors[index].name),
+                      subtitle: Text(filteredDoctors[index].city));
+                },
+              ),
+            )
+          ],
         ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: new TextField(
-            style: TextStyle(color: Colors.black, fontSize: 22),
-            decoration: InputDecoration(
-              hintText: "Search for a doctor",
-              hintStyle: TextStyle(color: Colors.black, fontSize: 22.0),
-            )),
-      ),
-      body: Center(
-        child: Text("Hello Search for a doctor"),
       ),
     );
   }
