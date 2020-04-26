76881c88e2c165416448500552bd48114c19d829
Pierre Edímo <pedimonkoe@yahoo.com>
2020-04-26 08:13:52 +0200

    i added a theme Model

---
+++ lib/Model/ThemeModel.dart
@@ -0,0 +1,24 @@
+import 'package:findadoctor/Model/ArticleModel.dart';
+import 'package:flutter/foundation.dart';
+
+class ThemeModel {
+  final int id;
+  final String name;
+  final String imageUrl;
+  List<Article> articles;
+
+  ThemeModel(
+      {@required this.id,
+      @required this.name,
+      @required this.imageUrl,
+      @required this.articles});
+
+  factory ThemeModel.fromJson(Map<String, dynamic> json) {
+    return ThemeModel(
+        id: json['id'] as int,
+        name: json['name'] as String,
+        imageUrl: json['imageUrl'] as String,
+        articles: List<Article>.from(
+            json['articles'].map((x) => Article.fromJson(x))));
+  }
+}
