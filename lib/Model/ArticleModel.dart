bfb0f8a4411f1172719e3e51eb47439e2ab7ad52
Pierre Edímo <pedimonkoe@yahoo.com>
2020-07-23 19:58:21 +0200

    i created  the author model to the article

--- lib/Model/ArticleModel.dart
+++ lib/Model/ArticleModel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 
+import 'Author.dart';
+
 class Article {
   final int id;
   final String title;
@@ -7,31 +9,25 @@ class Article {
   final String content;
   final int autorId;
   final String createdAt;
-  final String author;
-  final String biography;
-  final String authorImg;
 
-  Article(
-      {@required this.id,
-      @required this.title,
-      @required this.imageUrl,
-      @required this.content,
-      @required this.autorId,
-      @required this.createdAt,
-      @required this.author,
-      @required this.biography,
-      @required this.authorImg});
+  final Author author;
 
-  factory Article.fromJson(Map<String, dynamic> json) {
-    return Article(
-        id: json['id'] as int,
-        title: json['title'] as String,
-        imageUrl: json['imageUrl'] as String,
-        content: json['content'] as String,
-        autorId: json['autorId'] as int,
-        createdAt: json['createdAt'] as String,
-        author: json['author'] as String,
-        biography: json['biography'] as String,
-        authorImg: json['authorImg'] as String);
-  }
+  Article({
+    @required this.id,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.content,
+    @required this.autorId,
+    @required this.createdAt,
+    @required this.author,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      imageUrl: json['imageUrl'] as String,
+      content: json['content'] as String,
+      autorId: json['autorId'] as int,
+      createdAt: json['createdAt'] as String,
+      author: Author.fromJson(json['author']));
 }
