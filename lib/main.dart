11fed2a036e071fd1f0ca900833d8a79036a3970
Pierre Edímo <pedimonkoe@yahoo.com>
2020-07-23 19:57:32 +0200

    i added the display dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:Newsroom/UI/Login.dart';
 import 'package:Newsroom/UI/Profile.dart';
+import 'package:Newsroom/UI/ForumPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -7,7 +8,20 @@ import 'UI/ArticleList.dart';
 import 'UI/Theme.dart';
 
 final storage = FlutterSecureStorage();
-bool isLoggedInR;
+void displayDialog(context, title, text) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+        ),
+        content: Text(
+          text,
+          style: TextStyle(fontSize: 14.0),
+        ),
+      ),
+    );
 
 void main() => runApp(MyApp());
 
@@ -22,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'findadoctor',
+      title: 'newsroom',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           fontFamily: 'Monserrat', scaffoldBackgroundColor: Colors.white),
@@ -53,6 +67,7 @@ class _BottomNavigationState extends State<BottomNavigation> {
   final _widgetOptions = <Widget>[
     new ListArticlePage(),
     new ThemePage(),
+    new ForumPage(),
     new ProfilePage()
   ];
 
@@ -71,22 +86,22 @@ class _BottomNavigationState extends State<BottomNavigation> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: FaIcon(
-                FontAwesomeIcons.rss,
-                size: 18,
-              ),
+              icon: FaIcon(FontAwesomeIcons.rss, size: 18),
               title: Text("NewsFeed")),
           BottomNavigationBarItem(
               icon: FaIcon(FontAwesomeIcons.compass, size: 18),
               title: Text("Favorites")),
+          BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.penSquare, size: 18),
+              title: Text("Forum")),
           BottomNavigationBarItem(
               icon: FaIcon(FontAwesomeIcons.user, size: 18),
               title: Text("My Profile"))
         ],
         currentIndex: _selectedIndex,
         type: BottomNavigationBarType.shifting,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black12,
+        selectedItemColor: Colors.red.shade600,
+        unselectedItemColor: Colors.black,
         onTap: _onItemTapped,
         backgroundColor: Colors.white,
         showSelectedLabels: false,
