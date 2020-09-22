eef679dfa434c612f6318d6c4194f0683f3d7360
Pierre Edímo <pedimonkoe@yahoo.com>
2020-09-22 16:16:58 +0200

    i can delete a comment

--- lib/Service/CommentService.dart
+++ lib/Service/CommentService.dart
@@ -73,4 +73,67 @@ class CommentService {
     print(response.statusCode);
     return response.statusCode;
   }
+
+  Future<int> deleteComment(int id) async {
+    String jwt = await storage.read(key: "jwt");
+
+    final Response response = await delete(
+      'https://findadoc.azurewebsites.net/api/Comment/$id',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+    );
+
+    print(response.statusCode);
+    return response.statusCode;
+  }
+
+  Future<int> deleteAnswer(int id) async {
+    String jwt = await storage.read(key: "jwt");
+
+    final Response response = await delete(
+      'https://findadoc.azurewebsites.net/api/Answers/$id',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+    );
+
+    return response.statusCode;
+  }
+
+  Future<int> updateComment(
+    int id,
+    String uid,
+    String content,
+  ) async {
+    String jwt = await storage.read(key: "jwt");
+
+    final Response response = await put(
+      'https://findadoc.azurewebsites.net/api/Comment/$id',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      },
+      body: jsonEncode({'id': id, 'uid': uid, 'content': content}),
+    );
+
+    print(response.statusCode);
+    return response.statusCode;
+  }
+
+  Future<int> updateAnswer(int id, String content) async {
+    String jwt = await storage.read(key: "jwt");
+
+    final Response response = await put(
+        'https://findadoc.azurewebsites.net/api/Answers/$id',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + jwt
+        },
+        body: jsonEncode({'id': id, 'content': content}));
+
+    return response.statusCode;
+  }
 }
