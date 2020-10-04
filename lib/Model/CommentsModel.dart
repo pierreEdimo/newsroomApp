e0bda7697f7a4507398f991d516e65c741021db9
Pierre Edímo <pedimonkoe@yahoo.com>
2020-10-04 15:10:21 +0200

    i displayed the numberof answers in comments

--- lib/Model/CommentsModel.dart
+++ lib/Model/CommentsModel.dart
@@ -31,12 +31,14 @@ class GetCommentModel {
   final String uid;
   final UserModel author;
   final int articleId;
+  final int numberOfAnswers;
 
   GetCommentModel(
       {@required this.id,
       @required this.content,
       @required this.uid,
       @required this.author,
+      @required this.numberOfAnswers,
       @required this.articleId});
 
   factory GetCommentModel.fromJson(Map<String, dynamic> json) =>
@@ -45,6 +47,7 @@ class GetCommentModel {
         content: json['content'] as String,
         uid: json['uid'] as String,
         articleId: json['articleId'] as int,
+        numberOfAnswers: json['numberOfAnswers'] as int,
         author: UserModel.fromJson(json['author']),
       );
 }
