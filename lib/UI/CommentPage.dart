17c8a719ffdd80ec5b40ae6ef62cff2904f23a61
Pierre Edímo <pedimonkoe@yahoo.com>
2020-11-14 04:29:31 +0100

    commentPage redisign

--- lib/UI/CommentPage.dart
+++ lib/UI/CommentPage.dart
@@ -1,7 +1,7 @@
+import 'package:Newsroom/Component/Custom.Title.dart';
 import 'package:Newsroom/Model/CommentsModel.dart';
 import 'package:Newsroom/Service/CommentService.dart';
 import 'package:Newsroom/UI/AddCommentPage.dart';
-import 'package:Newsroom/UI/CommentDetail.dart';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -128,109 +128,114 @@ class _CommentPageState extends State<CommentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: FaIcon(
-            FontAwesomeIcons.arrowLeft,
-            size: 18,
-            color: Colors.black,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text(
-          "Comments",
-          style: TextStyle(
-            fontFamily: 'OpenSans',
-            color: Colors.black,
-          ),
-        ),
-      ),
-      body: filterComments.isEmpty
-          ? Center(
-              child: Text("No Comments"),
-            )
-          : Builder(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SafeArea(
+              child: Container(
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    listTitle("Comments", 22.0),
+                    IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () => Navigator.of(context).pop(),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Builder(
               builder: (BuildContext context) {
-                return ListView(
-                  children: filterComments
-                      .map(
-                        (GetCommentModel comment) => Container(
-                          margin: EdgeInsets.only(bottom: 10.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5LGSA_ar1nJAEJYCVNxoW77y4z-HGl0Yfug&usqp=CAU"),
-                                ),
-                                title: Text(
-                                  comment.author.userName,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                                subtitle: Text(
-                                  comment.author.profession,
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                trailing: IconButton(
-                                  icon: FaIcon(
-                                    FontAwesomeIcons.ellipsisV,
-                                    size: 18,
-                                    color: Colors.black,
-                                  ),
-                                  onPressed: () => showCommentModalSheet(
-                                      comment.uid, comment.id, context),
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () => Navigator.of(context)
-                                    .push(
-                                      MaterialPageRoute(
-                                        builder: (context) => CommentDetail(
-                                          commentId: comment.id,
-                                          commentContent: comment.content,
-                                        ),
-                                      ),
-                                    )
-                                    .then((_) => _fetchComments()),
-                                child: Container(
-                                  padding: const EdgeInsets.only(
-                                    left: 20,
-                                    right: 20,
-                                    bottom: 20,
-                                  ),
-                                  child: Text(
-                                    comment.content,
-                                    style: TextStyle(fontSize: 13.0),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 20.0, right: 20.0, bottom: 20.0),
-                                child: Text(
-                                  " ${comment.numberOfAnswers} Answer(s)",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.blue.shade600),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
+                return filterComments.isEmpty
+                    ? Center(
+                        child: Text("No Comments"),
                       )
-                      .toList(),
-                );
+                    : ListView(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.all(0.0),
+                        physics: ClampingScrollPhysics(),
+                        children: filterComments
+                            .map(
+                              (GetCommentModel comment) => Container(
+                                margin: EdgeInsets.only(bottom: 20.0),
+                                padding: EdgeInsets.all(20.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Colors.grey.shade200,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.8),
+                                      spreadRadius: 2,
+                                      blurRadius: 3,
+                                      offset: Offset(0, 2),
+                                    )
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5LGSA_ar1nJAEJYCVNxoW77y4z-HGl0Yfug&usqp=CAU"),
+                                            ),
+                                            SizedBox(
+                                              width: 10.0,
+                                            ),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  comment.author.userName,
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                                Text(comment.author.profession)
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.more_vert),
+                                          onPressed: () =>
+                                              showCommentModalSheet(comment.uid,
+                                                  comment.id, context),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Container(
+                                      child: Text(
+                                        comment.content,
+                                        style: TextStyle(fontSize: 16.0),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      );
               },
             ),
+          ],
+        ),
+      ),
       floatingActionButton: new FloatingActionButton.extended(
         backgroundColor: Colors.red.shade600,
         label: Text("write a comment"),
