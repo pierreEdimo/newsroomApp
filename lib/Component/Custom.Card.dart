33aa1bf540bc7e4750922b45e91a3bde845ca59c
Pierre Edímo <pedimonkoe@yahoo.com>
2020-11-14 04:26:19 +0100

    i created a custom widget

---
+++ lib/Component/Custom.Card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+Widget text(String text) {
+  return Text(
+    text,
+    style: TextStyle(color: Colors.white, fontSize: 13.0),
+  );
+}
+
+Widget customCard(String title, String imageUrl, double width, double fontSize,
+    double height, String authorImageUrl, String authorName, String entryDate) {
+  return Container(
+    child: Stack(
+      children: [
+        Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(imageUrl), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.8),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 2),
+              )
+            ],
+          ),
+        ),
+        Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+          ),
+        ),
+        Container(
+          width: width,
+          height: height,
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 30.0,
+                          height: 30.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50.0),
+                            image: DecorationImage(
+                                image: NetworkImage(authorImageUrl),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        text(authorName)
+                      ],
+                    ),
+                    text(entryDate)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                alignment: Alignment.bottomLeft,
+                height: 58,
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'OpenSans',
+                        fontSize: fontSize),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
