4eb6f6f2be2ab294f914d05ccaa46be3c6328dbc
pierre edimo <pierredimo@live.com>
2020-11-21 20:50:21 -0800

    minor improvements

--- lib/Service/CommentService.dart
+++ lib/Service/CommentService.dart
@@ -1,4 +1,3 @@
-import 'package:Newsroom/Model/Answer.dart';
 import 'package:Newsroom/Model/CommentsModel.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
@@ -22,24 +21,6 @@ class CommentService {
     return response.statusCode;
   }
 
-  Future<List<GetAnsWer>> fetchAnswers() async {
-    String jwt = await storage.read(key: "jwt");
-    Response response = await get(
-        'https://findadoc.azurewebsites.net/api/Answers',
-        headers: {'Authorization': 'Bearer ' + jwt});
-
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-
-      List<GetAnsWer> answers =
-          body.map((dynamic item) => GetAnsWer.fromJson(item)).toList();
-
-      return answers;
-    } else {
-      throw "error";
-    }
-  }
-
   Future<List<GetCommentModel>> getComments() async {
     String jwt = await storage.read(key: "jwt");
     Response response = await get(
@@ -56,22 +37,6 @@ class CommentService {
     } else {
       throw "error";
     }
-  }
-
-  Future<int> ansWerComment(AnsWer answerComment) async {
-    String jwt = await storage.read(key: "jwt");
-    String jsEncode;
-    jsEncode = jsonEncode(answerComment);
-    final Response response = await post(
-        'https://findadoc.azurewebsites.net/api/Answers',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + jwt
-        },
-        body: jsEncode);
-
-    print(response.statusCode);
-    return response.statusCode;
   }
 
   Future<int> deleteComment(int id) async {
