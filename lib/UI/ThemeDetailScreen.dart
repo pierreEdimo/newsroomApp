cae41c07f19f72342756d0889ef25c82a0e2b948
pierre edimo <pierredimo@live.com>
2021-01-10 04:07:10 +0100

    it shows a message when there are no items in a category

--- lib/UI/ThemeDetailScreen.dart
+++ lib/UI/ThemeDetailScreen.dart
@@ -1,5 +1,6 @@
 import 'package:Newsroom/Component/Custom.Card.dart';
-import 'package:Newsroom/Component/Custom.Title.dart';
+import 'package:Newsroom/Component/EmptyArray.dart';
+import 'package:Newsroom/Component/HeaderDetail.dart';
 import 'package:Newsroom/Model/ArticleModel.dart';
 import 'package:Newsroom/Model/ThemeModel.dart';
 import 'package:Newsroom/Service/ThemeService.dart';
@@ -8,100 +9,93 @@ import 'package:flutter/material.dart';
 
 class ThemeDetailScreen extends StatefulWidget {
   final int themeId;
+  final String themeName;
 
-  ThemeDetailScreen({@required this.themeId});
+  ThemeDetailScreen({@required this.themeId, @required this.themeName});
   @override
   _ThemeDetailScreenState createState() =>
-      _ThemeDetailScreenState(themeId: themeId);
+      _ThemeDetailScreenState(themeId: themeId, themeName: themeName);
 }
 
 class _ThemeDetailScreenState extends State<ThemeDetailScreen> {
   ThemeService _themeService = ThemeService();
-  Future<ThemeModel> themeModel;
+  Future<ThemeModel> _themeModel;
   int themeId;
+  String themeName;
 
-  _ThemeDetailScreenState({@required this.themeId});
+  _ThemeDetailScreenState({@required this.themeId, @required this.themeName});
 
   @override
   void initState() {
     super.initState();
-    _fetchSingleTheme();
-  }
-
-  _fetchSingleTheme() {
-    themeModel = _themeService.getAThemebyId(themeId);
+    _themeModel = _themeService.getAThemebyId(themeId);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          padding: EdgeInsets.all(20.0),
-          child: Container(
-            child: FutureBuilder(
-              future: themeModel,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  ThemeModel themeModel = snapshot.data;
-
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SafeArea(
-                        child: Container(
-                          height: 60,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              listTitle(themeModel.name, 22.0),
-                              IconButton(
-                                icon: Icon(Icons.close),
-                                onPressed: () => Navigator.of(context).pop(),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Container(
-                        child: ListView(
-                            shrinkWrap: true,
-                            physics: ClampingScrollPhysics(),
-                            padding: EdgeInsets.all(0.0),
-                            children: themeModel.articles
-                                .map((Article article) => InkWell(
-                                      onTap: () => Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  ArticleDetailScreen(
-                                                      articleId: article.id,
-                                                      authorId:
-                                                          article.autorId))),
-                                      child: Container(
-                                        margin: EdgeInsets.only(bottom: 20.0),
-                                        child: customCard(
-                                            article.title,
-                                            article.imageUrl,
-                                            400,
-                                            20.0,
-                                            250,
-                                            article.author.imageUrl,
-                                            article.author.name,
-                                            article.createdAt),
-                                      ),
-                                    ))
-                                .toList()),
-                      )
-                    ],
-                  );
-                }
-                return Text("");
-              },
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SafeArea(
+              child: headerDetail(themeName, context),
             ),
-          )),
+            SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              child: Container(
+                child: FutureBuilder(
+                  future: _themeModel,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      ThemeModel themeModel = snapshot.data;
+
+                      return themeModel.articles.length < 1
+                          ? Center(
+                              child: emptyArray(
+                                  "there are no Articles on this topic yet , please try again later",
+                                  context),
+                            )
+                          : ListView(
+                              padding: EdgeInsets.all(0.0),
+                              children: themeModel.articles
+                                  .map((Article article) => InkWell(
+                                        onTap: () => Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ArticleDetailScreen(
+                                                        articleId: article.id,
+                                                        authorId:
+                                                            article.autorId))),
+                                        child: Container(
+                                          margin: EdgeInsets.only(bottom: 20.0),
+                                          child: customCard(
+                                              article.title,
+                                              article.imageUrl,
+                                              400,
+                                              20.0,
+                                              250,
+                                              article.author.imageUrl,
+                                              article.author.name,
+                                              article.createdAt),
+                                        ),
+                                      ))
+                                  .toList());
+                    }
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
