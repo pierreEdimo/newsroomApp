516b64c68b06a8afa8fd164594d38d28d2b14351
pierre edimo <pierredimo@live.com>
2021-01-10 04:14:10 +0100

    i rearranged the  articlescreen

--- lib/UI/ArticleScreen.dart
+++ lib/UI/ArticleScreen.dart
@@ -1,15 +1,12 @@
 import 'package:Newsroom/Component/Custom.Card.dart';
-import 'package:Newsroom/Component/Custom.Title.dart';
-import 'package:Newsroom/Component/Theme.Card.dart';
+
+import 'package:Newsroom/Component/Header.dart';
+
 import 'package:Newsroom/Model/ArticleModel.dart';
-import 'package:Newsroom/Model/ThemeModel.dart';
 
 import 'package:Newsroom/Service/ArticleService.dart';
 
-import 'package:Newsroom/Service/ThemeService.dart';
 import 'package:Newsroom/UI/ArticleDetailScreen.dart';
-
-import 'package:Newsroom/UI/ThemeDetailScreen.dart';
 
 import 'package:flutter/material.dart';
 
@@ -22,135 +19,87 @@ class _ArticleSreenState extends State<ArticleSreen> {
   Future<List<Article>> _articles;
   final ArticleService _articleService = ArticleService();
 
-  Future<List<ThemeModel>> _themes;
-  final ThemeService _themeService = ThemeService();
   String _url =
-      "https://findadoc.azurewebsites.net/api/Article?sortBy=title&sortOrder=desc&Size=15";
+      "https://findadoc.azurewebsites.net/api/Article?sortBy=title&sortOrder=desc&Size=20";
 
   @override
   void initState() {
     super.initState();
     _fetchArticles();
-    _fetchThemes();
   }
 
   _fetchArticles() {
     _articles = _articleService.getArticles(_url);
   }
 
-  _fetchThemes() {
-    _themes = _themeService.getAllThemes();
+  Future<void> _loadArticles() async {
+    setState(() {
+      _fetchArticles();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: Container(
         padding: EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SafeArea(
+              child: header("Latest News", context),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
               child: Container(
-                child: listTitle("hot Topics", 22.0),
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 200,
-              child: FutureBuilder(
-                future: _themes,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<ThemeModel>> snapshot) {
-                  if (snapshot.hasData) {
-                    List<ThemeModel> themes = snapshot.data;
+                child: RefreshIndicator(
+                  onRefresh: _loadArticles,
+                  child: FutureBuilder(
+                    future: _articles,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Article>> snapshot) {
+                      if (snapshot.hasData) {
+                        List<Article> articles = snapshot.data;
 
-                    return ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: themes
-                          .map((ThemeModel theme) => InkWell(
-                                onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        ThemeDetailScreen(themeId: theme.id),
+                        return ListView(
+                          padding: EdgeInsets.all(0.0),
+                          children: articles
+                              .map(
+                                (Article article) => InkWell(
+                                  onTap: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ArticleDetailScreen(
+                                        articleId: article.id,
+                                        authorId: article.autorId,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Container(
+                                    margin: EdgeInsets.only(bottom: 20.0),
+                                    child: customCard(
+                                        article.title,
+                                        article.imageUrl,
+                                        400,
+                                        20.0,
+                                        250,
+                                        article.author.imageUrl,
+                                        article.author.name,
+                                        article.createdAt),
                                   ),
                                 ),
-                                child: Container(
-                                  margin: EdgeInsets.only(right: 20.0),
-                                  child: themeCard(
-                                    theme.name,
-                                    theme.imageUrl,
-                                    150,
-                                    16.0,
-                                    200,
-                                  ),
-                                ),
-                              ))
-                          .toList(),
-                    );
-                  }
-                  return Center(
-                    child: Text(""),
-                  );
-                },
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            listTitle('Latest News', 18.0),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              child: FutureBuilder(
-                future: _articles,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<Article>> snapshot) {
-                  if (snapshot.hasData) {
-                    List<Article> articles = snapshot.data;
-
-                    return ListView(
-                      shrinkWrap: true,
-                      primary: false,
-                      padding: EdgeInsets.all(0.0),
-                      physics: ClampingScrollPhysics(),
-                      children: articles
-                          .map(
-                            (Article article) => InkWell(
-                              onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ArticleDetailScreen(
-                                    articleId: article.id,
-                                    authorId: article.autorId,
-                                  ),
-                                ),
-                              ),
-                              child: Container(
-                                margin: EdgeInsets.only(bottom: 20.0),
-                                child: customCard(
-                                    article.title,
-                                    article.imageUrl,
-                                    400,
-                                    20.0,
-                                    250,
-                                    article.author.imageUrl,
-                                    article.author.name,
-                                    article.createdAt),
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    );
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
+                              )
+                              .toList(),
+                        );
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
+                  ),
+                ),
               ),
             ),
           ],
