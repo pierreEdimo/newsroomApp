3a3e5fb3ced8390f269325889e60078dcd127ea6
pierre edimo <pierredimo@live.com>
2021-01-10 04:04:13 +0100

    small improv

--- lib/Model/LoginModel.dart
+++ lib/Model/LoginModel.dart
@@ -4,10 +4,18 @@ class LoginModel {
   final String email;
   final String password;
 
-  LoginModel({@required this.email, @required this.password});
+  LoginModel({
+    @required this.email,
+    @required this.password,
+  });
 
   factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
-      email: json['email'] as String, password: json['password'] as String);
+        email: json['email'] as String,
+        password: json['password'] as String,
+      );
 
-  Map<String, dynamic> toJson() => {"email": email, "password": password};
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "password": password,
+      };
 }
