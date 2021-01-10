bcffd6eafb50cb144724ec421b669728bbb3fe10
pierre edimo <pierredimo@live.com>
2021-01-10 04:07:36 +0100

    i deleted the profession variable

--- lib/Model/UserModel.dart
+++ lib/Model/UserModel.dart
@@ -4,24 +4,22 @@ class UserModel {
   final String userName;
   final String passWord;
   final String email;
-  final String profession;
 
-  UserModel(
-      {@required this.userName,
-      @required this.passWord,
-      @required this.email,
-      @required this.profession});
+  UserModel({
+    @required this.userName,
+    @required this.passWord,
+    @required this.email,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-      userName: json['userName'] as String,
-      passWord: json['passWord'] as String,
-      email: json['email'] as String,
-      profession: json['profession'] as String);
+        userName: json['userName'] as String,
+        passWord: json['passWord'] as String,
+        email: json['email'] as String,
+      );
 
   Map<String, dynamic> toJson() => {
         "userName": userName,
         "passWord": passWord,
         "email": email,
-        "profession": profession
       };
 }
