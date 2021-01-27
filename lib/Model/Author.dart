f3f6ae4b7b4e0a4481d59a15ee249a760677b429
pierre edimo <pierredimo@live.com>
2021-01-27 16:41:18 +0100

    remove imageurl variable

--- lib/Model/Author.dart
+++ lib/Model/Author.dart
@@ -4,17 +4,16 @@ class Author {
   final int id;
   final String name;
   final String biography;
-  final String imageUrl;
 
-  Author(
-      {@required this.biography,
-      @required this.id,
-      @required this.imageUrl,
-      @required this.name});
+  Author({
+    @required this.biography,
+    @required this.id,
+    @required this.name,
+  });
 
   factory Author.fromJson(Map<String, dynamic> json) => Author(
-      biography: json['biography'] as String,
-      id: json['id'] as int,
-      name: json['name'] as String,
-      imageUrl: json['imageUrl'] as String);
+        biography: json['biography'] as String,
+        id: json['id'] as int,
+        name: json['name'] as String,
+      );
 }
