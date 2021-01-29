a22492529f716ac970be64c1d466be221fc87ee3
pierre edimo <pierredimo@live.com>
2021-01-29 23:57:06 +0100

    add errormessage

---
+++ lib/Component/Center.Error.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget centerError() {
+  return Center(
+    child: Text(
+      "Error , you are not connected to the internet , please try again",
+      textAlign: TextAlign.center,
+    ),
+  );
+}
