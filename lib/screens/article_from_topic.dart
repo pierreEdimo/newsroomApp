2b83b7d72ecf804176fccbb6709511e939ec2501
pierre edimo <pierredimo@live.com>
2021-04-12 19:37:52 +0200

    add articlefromtopicScreen

---
+++ lib/screens/article_from_topic.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:newsroom/widget/custom_app_bar.dart';
+import 'package:newsroom/widget/list_of_articles.dart';
+import 'package:newsroom/widget/title_row.dart';
+
+class ArticleFromTopic extends StatelessWidget {
+  final int topicId;
+  final String topicName;
+
+  ArticleFromTopic({
+    @required this.topicId,
+    @required this.topicName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        height: 100,
+        child: titleRow(
+          topicName,
+          IconButton(
+            icon: Icon(Icons.close_outlined),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          context,
+        ),
+      ),
+      body: listOfArticles(
+        "https://newsplace.azurewebsites.net/api/articles/Filter?TopicId=$topicId",
+        context,
+      ),
+    );
+  }
+}
