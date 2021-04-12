43e0bc6cb30746eff57f295e4f1938c146968acb
pierre edimo <pierredimo@live.com>
2021-04-12 19:34:56 +0200

    add comment model

---
+++ lib/model/comment.dart
@@ -0,0 +1,27 @@
+import 'package:newsroom/model/userModel.dart';
+
+class Comment {
+  final int id;
+  final String createdAt;
+  final int articleId;
+  final UserModel author;
+  final String content;
+  final String authorId;
+
+  Comment(
+      {this.articleId,
+      this.id,
+      this.content,
+      this.author,
+      this.createdAt,
+      this.authorId});
+
+  factory Comment.fromJson(Map<String, dynamic> json) => Comment(
+        articleId: json['articleId'] as int,
+        content: json['content'] as String,
+        id: json['id'] as int,
+        createdAt: json['createdAt'] as String,
+        authorId: json['authorId'] as String,
+        author: UserModel.fromJson(json['author']),
+      );
+}
