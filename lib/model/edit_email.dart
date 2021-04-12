9c4be9c62fd7cef8d88f5285060dacc75c641552
pierre edimo <pierredimo@live.com>
2021-04-12 19:35:10 +0200

    add email model

---
+++ lib/model/edit_email.dart
@@ -0,0 +1,13 @@
+class EditEmail {
+  final String email;
+
+  EditEmail({this.email});
+
+  factory EditEmail.fromJson(Map<String, dynamic> json) => EditEmail(
+        email: json["email"] as String,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+      };
+}
