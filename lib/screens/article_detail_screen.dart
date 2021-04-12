99a5884a8ca503af2c9dd7819bdd65bc9f50c079
pierre edimo <pierredimo@live.com>
2021-04-12 19:37:30 +0200

    add articleDetailScreen

---
+++ lib/screens/article_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:newsroom/widget/article_detail_container.dart';
+
+class ArticleDetail extends StatelessWidget {
+  final int id;
+
+  ArticleDetail({@required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: articleDetailContainer(
+          id,
+          context,
+        ),
+      ),
+    );
+  }
+}
