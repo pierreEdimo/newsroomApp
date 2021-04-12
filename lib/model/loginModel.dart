42b224ff7c81c3641fa746984706aa8e70b4e011
pierre edimo <pierredimo@live.com>
2021-04-12 19:35:31 +0200

    add loginmodel

---
+++ lib/model/loginModel.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+class LoginModel {
+  final String email;
+  final String password;
+
+  LoginModel({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
+        email: json['email'] as String,
+        password: json['password'] as String,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+        "password": password,
+      };
+}
