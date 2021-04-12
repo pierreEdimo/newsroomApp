0af8f4ebe9d56cba2831493f29fca6d54ba5bf34
pierre edimo <pierredimo@live.com>
2021-04-12 19:37:14 +0200

    add addCommentScreen

---
+++ lib/screens/add_comment_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:newsroom/main.dart';
+import 'package:newsroom/model/add_comment.dart';
+import 'package:newsroom/service/comment_service.dart';
+import 'package:newsroom/widget/comment_input.dart';
+import 'package:newsroom/widget/custom_app_bar.dart';
+import 'package:provider/provider.dart';
+
+class AddCommentScreen extends StatelessWidget {
+  final int articleId;
+
+  AddCommentScreen({@required this.articleId});
+
+  final maxLines = 26;
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        height: 100,
+        child: SafeArea(
+          child: Center(
+            child: Container(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 25.0),
+                child: IconButton(
+                  icon: Icon(Icons.close_outlined),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        onPressed: () async {
+          var uid = await storage.read(key: "userId");
+          if (_controller.text.isNotEmpty) {
+            AddComment comment = new AddComment(
+              content: _controller.text,
+              articleId: articleId,
+              authorId: uid,
+            );
+            Provider.of<CommentService>(context, listen: false)
+                .addComment(comment)
+                .then((_) => _controller.text = "")
+                .then(
+                  (_) => Navigator.of(context).pop(),
+                );
+          }
+          DoNothingAction();
+        },
+        label: Text(
+          "Post",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Padding(
+        padding: EdgeInsets.only(
+          right: 25.0,
+          left: 25.0,
+          top: 10.0,
+          bottom: 10.0,
+        ),
+        child: Container(
+          height: maxLines * 24.0,
+          child: commentInput(_controller, maxLines),
+        ),
+      ),
+    );
+  }
+}
