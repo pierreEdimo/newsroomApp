f2aac61878627b334b1a35f2809ef41dc72dfd86
pierre edimo <pierredimo@live.com>
2021-04-12 19:36:10 +0200

    add topicmodel

---
+++ lib/model/topic.dart
@@ -0,0 +1,17 @@
+class Topic {
+  final int id;
+  final String name;
+  final String imageUrl;
+
+  Topic({
+    this.id,
+    this.imageUrl,
+    this.name,
+  });
+
+  factory Topic.fromJson(Map<String, dynamic> json) => Topic(
+        id: json['id'] as int,
+        name: json['name'] as String,
+        imageUrl: json['imageUrl'] as String,
+      );
+}
