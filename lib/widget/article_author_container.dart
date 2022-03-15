8848e16abd7445b7681b924d06a23720cc40c7d0
pierreEdimo <pedimonkoe@yahoo.com>
2022-03-16 00:32:19 +0100

    last update

--- lib/widget/article_author_container.dart
+++ lib/widget/article_author_container.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:newsroom/model/article.dart';
 import 'package:newsroom/utilities/constants.dart';
 
-Widget articleRow(Article article) {
+Widget articleAuthor(Article article) {
   return Text(
-    "${article.author!.name!} . ${article.createdAt!}",
+    "${article.author!.name!}",
     style: nameAndDateStyle,
   );
 }
---
+++ lib/widget/icon_container.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:newsroom/screens/search_screen.dart';
+
+class IconContainer extends StatelessWidget {
+  const IconContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
